06696b23c8ed942688b14cc12e5a7090d07d3dfb
TopScrech <sergei_saliukov@icloud.com>
2026-01-21 22:46:20 +0100

    Initial Commit

---
+++ FanControl/FanControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanControlApp.swift
+//  FanControl
+//
+//  Created by Sergei Saliukov on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FanControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
