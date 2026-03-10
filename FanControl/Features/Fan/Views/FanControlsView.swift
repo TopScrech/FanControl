0197dc8081b3a03bd699a4ada19568342279d771
TopScrech <sergei_saliukov@icloud.com>
2026-03-10 14:22:47 +0100

    clean up

--- FanControl/Features/Fan/Views/FanControlsView.swift
+++ FanControl/Features/Fan/Views/FanControlsView.swift
@@ -23,27 +23,7 @@ struct FanControlsView: View {
                     FanDetailsCardView(fan: fan)
                 }
                 
-                FanActionCardView(
-                    canSetManual: model.controlMinRPM != nil && model.controlMaxRPM != nil,
-                    canUsePresets: model.canUsePresetControl,
-                    presetRPMs: model.controlPresetRPMs,
-                    sensors: model.temperatureSensors,
-                    selectedCustomPreset: model.selectedCustomPresetDraft,
-                    isCustomPresetActive: model.selectedCustomPresetIsActive,
-                    customPresetPercentageText: model.selectedCustomPresetPercentageText,
-                    activeMode: model.activeControlMode,
-                    isSendingAttempts: model.showsControlAttemptProgress
-                ) {
-                    Task { await model.setAuto() }
-                } setMin: {
-                    Task { await model.setControlMin() }
-                } setFull: {
-                    Task { await model.setControlMax() }
-                } setPreset: { rpm in
-                    Task { await model.setManualRPM(Double(rpm)) }
-                } setCustomPreset: { draft in
-                    Task { await model.setCustomPreset(draft) }
-                }
+                FanActionCardView(model: model)
             }
             
             if showSensors {
