2c69cc1c4e2dd67459c2f8485aea77dd674600bf
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-09-15 21:18:51 +0200

    updated App Store link

--- FanControl/Features/Fan/Views/FanPromotionsView.swift
+++ FanControl/Features/Fan/Views/FanPromotionsView.swift
@@ -3,10 +3,10 @@ import ScrechKit
 struct FanPromotionsView: View {
     @AppStorage("hasDismissedDiscordPromotion") private var hasDismissedDiscordPromotion = false
     @AppStorage("hasDismissedIPhonePromotion") private var hasDismissedIPhonePromotion = false
-
+    
     private let discordURL = URL(string: "https://discord.gg/MAgTjeFTB7")
-    private let appStoreURL = URL(string: "https://apps.apple.com/us/app/fancontrol-remote/id6806835921")
-
+    private let appStoreURL = URL(string: "https://apps.apple.com/app/id6806835921")
+    
     var body: some View {
         Group {
             if !hasDismissedDiscordPromotion {
@@ -18,7 +18,7 @@ struct FanPromotionsView: View {
                     hasDismissedDiscordPromotion = true
                 }
             }
-
+            
             if !hasDismissedIPhonePromotion {
                 FanPromotionCard(
                     title: "Manage fan speed from your iPhone!",
